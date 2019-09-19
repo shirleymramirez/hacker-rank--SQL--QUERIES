47696ccc99b6017e0b3642fb2acabb68e9a9e32e
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-09-19 10:52:42 -0700

    Create weather-observation-7.sql

---
+++ weather-observation-7.sql
@@ -0,0 +1,21 @@
+/* Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '[aeiouAEIOU]$' ORDER BY CITY;
