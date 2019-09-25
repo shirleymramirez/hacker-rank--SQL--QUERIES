2ff76619f87fa3529ff2f1e0101169382b0a3bdd
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-09-25 13:30:17 -0700

    Create weather-observation-8.sql

---
+++ weather-observation-8.sql
@@ -0,0 +1,24 @@
+/* Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. 
+Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiouAEIOU].+[aeiouAEIOU]$' ORDER BY CITY;
+
